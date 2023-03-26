0e356e79c1500a49f5f60bc6903c9c0e740e0aef
Emre Yildirim <temreyildirim8@gmail.com>
2023-03-26 18:52:39 +0300

    Correct the layout

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,3 @@
 export const MILLENNIUM = 2000;
+export const CURRENT_YEAR = new Date().getFullYear();
+export const START_MIN = 1900;
--- src/pages/search/SearchPage.js
+++ src/pages/search/SearchPage.js
@@ -1,27 +1,32 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { MILLENNIUM } from "../../constants";
+import { CURRENT_YEAR, MILLENNIUM, START_MIN } from "../../constants";
 import "./SearchPage.css";
 
 const SearchPage = () => {
   const [query, setQuery] = useState("");
   const [startYear, setStartYear] = useState(MILLENNIUM);
-  const [endYear, setEndYear] = useState(new Date().getFullYear());
+  const [endYear, setEndYear] = useState(CURRENT_YEAR);
   const [searchResults, setSearchResults] = useState([]);
+  const [searchStarted, setSearchStarted] = useState(false);
 
   const handleQueryChange = (event) => {
+    setSearchStarted(false);
     setQuery(event.target.value);
   };
 
   const handleStartYearChange = (event) => {
+    setSearchStarted(false);
     setStartYear(event.target.value);
   };
 
   const handleEndYearChange = (event) => {
+    setSearchStarted(false);
     setEndYear(event.target.value);
   };
 
   const handleSearch = async () => {
+    setSearchStarted(true);
     try {
       const response = await axios.get(
         `https://images-api.nasa.gov/search?q=${query}&media_type=image&year_start=${startYear}&year_end=${endYear}`
@@ -34,7 +39,7 @@ const SearchPage = () => {
   };
 
   const renderSearchResults = () => {
-    return searchResults.map((result) => {
+    return searchResults?.map((result) => {
       const data = result.data[0];
       const title = data.title;
       const location = data.location;
@@ -66,16 +71,21 @@ const SearchPage = () => {
         </div>
         <div className="form-group">
           <label htmlFor="start-year" className="form-label">Start Year:</label>
-          <input type="number" min="1900" max="2099" id="start-year" value={startYear} onChange={handleStartYearChange} className="form-input"/>
+          <input type="number" min={START_MIN} max={CURRENT_YEAR} id="start-year" value={startYear} onChange={handleStartYearChange} className="form-input"/>
         </div>
         <div className="form-group">
           <label htmlFor="end-year" className="form-label">End Year:</label>
-          <input type="number" min="1900" max="2099" id="end-year" value={endYear} onChange={handleEndYearChange} className="form-input"/>
+          <input type="number" min={START_MIN} max={CURRENT_YEAR} id="end-year" value={endYear} onChange={handleEndYearChange} className="form-input"/>
         </div>
         <button onClick={handleSearch} disabled={!query || !startYear || !endYear} className="search-button">Search</button>
       </div>
       <div className="search-results">
-        {renderSearchResults()}
+        {searchResults.length > 0 ? (
+            renderSearchResults()
+          ) : (
+            <p> {searchStarted && "No results found." }</p>
+          )
+        }
       </div>
     </div>
   );
